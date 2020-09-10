f6a2d6c7d4d18601010ff18696373be237503c5a
allenkomban <38334264+allenkomban@users.noreply.github.com>
2020-09-10 15:44:40 +1000

    Add files via upload

---
+++ implementation.py
@@ -0,0 +1,58 @@
+from pyspark import SparkContext, SparkConf
+from time import time
+import pickle
+import submission
+import random
+
+
+
+
+
+ def createSC():
+     conf = SparkConf()
+     conf.setMaster("local[*]")
+     conf.setAppName("C2LSH")
+     sc = SparkContext(conf = conf)
+     return sc
+
+ with open("toy/toy_hashed_data", "rb") as file:
+     data = pickle.load(file)
+
+ with open("toy/toy_hashed_query", "rb") as file:
+     query_hashes = pickle.load(file)
+
+ alpha_m  = 10
+ beta_n = 10
+
+ sc=createSC()
+ data_hashes = sc.parallelize([(index, x) for index, x in enumerate(data)])
+ print("data_hash",data_hashes.collect())
+ print("query_hashes",query_hashes)
+ start_time = time()
+ res = submission.c2lsh(data_hashes, query_hashes, alpha_m, beta_n).collect()
+ end_time = time()
+ sc.stop()
+ print('alpha_m',alpha_m)
+ print('beta_n',beta_n)
+ print('running time:', end_time - start_time)
+ print('Number of candidate: ', len(res))
+ print('set of candidate: ', set(res))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
